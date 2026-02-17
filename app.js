6e296263e84ccdf5831dfb3be221dd343b60c6ba
gerthor <ger.thor97@gmail.com>
2026-02-17 10:52:36 -0800

    Navigation Bar, placeholder text, some skeleton frameworks finished

---
+++ app.js
@@ -0,0 +1,8 @@
+// Goal: Using searchbar, type in "fast" and pull up the first 6 movies with "fast" in the title, want to show the "title" and the "year", "image"
+// add a skeleton loading state, the spinning loading logo
+
+// http://www.omdbapi.com/?i=tt3896198&apikey=df6f10c4
+
+// OMDb API KEY: apikey=df6f10c4
+
+// https://www.omdbapi.com/?apikey=df6f10c4&s=fast ***Use this as cross-reference for confirming JavaScript results***
