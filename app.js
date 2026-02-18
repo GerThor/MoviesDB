5bbe8c20c8bf76489befbc7b78c9e46ebde7fa49
gerthor <ger.thor97@gmail.com>
2026-02-17 18:52:45 -0800

    Finished Files

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-// Goal: Using searchbar, type in "fast" and pull up the first 6 movies with "fast" in the title, want to show the "title" and the "year", "image"
-// add a skeleton loading state, the spinning loading logo
 
-// http://www.omdbapi.com/?i=tt3896198&apikey=df6f10c4
+async function fetchMovieAPI (searchQuery) {
+    const movieListElement = document.querySelector(".movie__container--list")
+    movieListElement.classList += " loading__list"
+    const movies = await fetch(`https://www.omdbapi.com/?apikey=df6f10c4&s=${searchQuery}`); 
+    const moviesData = await movies.json();
+    movieListElement.classList.remove("loading__list")
+    const sixMoviesList = moviesData.Search.slice(0, 6);
+    console.log(moviesData.Search);
+    movieListElement.innerHTML = sixMoviesList.map((movie) => movieHTML(movie)).join("");
 
-// OMDb API KEY: apikey=df6f10c4
+}
 
-// https://www.omdbapi.com/?apikey=df6f10c4&s=fast ***Use this as cross-reference for confirming JavaScript results***
+function movieHTML (movie) {
+    return `<div class="movie">
+        <img src="${movie.Poster}" alt="">
+        <h3 class="movie__title--year">${movie.Title} (${movie.Year})</h3>
+    </div>`;
+}
+
+function checkFormSubmission (event) {
+    event.preventDefault();
+    const searchQuery = document.getElementById("search__bar--input").value;
+    console.log("Search string is: ", searchQuery);
+    fetchMovieAPI(searchQuery);
+    const searchResultElement = document.querySelector(".movies__search-header")
+    searchResultElement.innerHTML = displaySearchKeyword(searchQuery);
+}
+
+function displaySearchKeyword (searchQuery) {
+    return `<h2>Search Results: <span style="color:blue">"${searchQuery}"</span></h2>`
+}
