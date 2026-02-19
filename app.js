1742dd1f07c5ec69bb0ef0ab3bb5bc9cc7f53063
gerthor <ger.thor97@gmail.com>
2026-02-18 18:12:31 -0800

    Implemented footer, resolution scaling, hamburger menu

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+/* MOVIES API */
 async function fetchMovieAPI (searchQuery) {
     const movieListElement = document.querySelector(".movie__container--list")
     movieListElement.classList += " loading__list"
@@ -6,9 +6,9 @@ async function fetchMovieAPI (searchQuery) {
     const moviesData = await movies.json();
     movieListElement.classList.remove("loading__list")
     const sixMoviesList = moviesData.Search.slice(0, 6);
-    console.log(moviesData.Search);
+    // console.log(moviesData.Search);
     movieListElement.innerHTML = sixMoviesList.map((movie) => movieHTML(movie)).join("");
-
+    
 }
 
 function movieHTML (movie) {
@@ -21,7 +21,6 @@ function movieHTML (movie) {
 function checkFormSubmission (event) {
     event.preventDefault();
     const searchQuery = document.getElementById("search__bar--input").value;
-    console.log("Search string is: ", searchQuery);
     fetchMovieAPI(searchQuery);
     const searchResultElement = document.querySelector(".movies__search-header")
     searchResultElement.innerHTML = displaySearchKeyword(searchQuery);
@@ -29,4 +28,43 @@ function checkFormSubmission (event) {
 
 function displaySearchKeyword (searchQuery) {
     return `<h2>Search Results: <span style="color:blue">"${searchQuery}"</span></h2>`
+}
+
+function filterMovies(event) {
+
+    const sortMovies = document.getElementById("dropdown__sort--bar");
+    const movies = Array.from(document.querySelectorAll(".movie"))
+    for(let i = 0; i < movies.length; i++) {
+        console.log("unsorted movies", movies[i].textContent);
+    }
+
+
+    movies.sort((a, b) => {
+        const movieA = a.querySelector('h3').textContent.toLocaleLowerCase()
+        const movieB = b.querySelector('h3').textContent.toLocaleLowerCase()
+
+        if (sortMovies === "A_to_Z") {
+            return movieA.localeCompare(movieB)
+        } else if (sortMovies === "Z_to_A"){
+            return movieB.localeCompare(movieA)
+        }
+    })
+
+    
+    const parent = movies[0].parentNode;
+    parent.innerHTML = "";
+    movies.forEach(movie => parent.appendChild(movie));
+    for(let i = 0; i < movies.length; i++) {
+        console.log("sorted movies", movies[i].textContent);
+    }
+}
+
+/* HAMBURGER MENU */
+
+function openMenu() {
+    document.body.classList += " menu--open";
+}
+
+function closeMenu() {
+    document.body.classList.remove("menu--open")
 }
